8914604d2c5260596fb3f63146ddc1d8e22e1295
Rahul Palve <hi.rahulpalve@gmail.com>
2019-09-12 13:43:15 +0530

    Solving Failed Deploy

--- todo/settings.py
+++ todo/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 import dj_database_url 
