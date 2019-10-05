ec59ea3dfc127d8c2624ffd56434f27cead5bc84
paulo <paulo.o.medeiros@hotmail.com>
2019-10-05 15:57:01 -0300

    01 See all page

--- todoapp/views.py
+++ todoapp/views.py
@@ -9,9 +9,13 @@ def show_todo(request,link_c):
     todo=Todo.objects.get(link_code=link_c)
     return render(request, "detail.html", {'todo': todo,'todos':todos})
 
+def all(request):
+    todos = Todo.objects.all()
+    return render(request, 'all.html', {'todos': todos})
+
 def index(request):
     todos=Todo.objects.order_by('-date')[:8]
-    todos=Todo.objects.all()
+    todos=Todo.objects.all()[:8]
     form=todoForm()
     if request.method=="POST":
         form=todoForm(request.POST)
